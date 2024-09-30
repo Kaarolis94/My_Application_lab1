5a7fe03f69cb376ad0265b7a5b096cb91fe9468f
Kaarolis94 <karolis.zokas@gmail.com>
2024-09-30 11:27:51 +0300

    First Commit

--- app/src/main/java/com/example/myapplication_lab1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_lab1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication_lab1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Find the Button and TextView by their IDs
+        val button: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.textView)
+
+        // Set an OnClickListener on the Button
+        button.setOnClickListener {
+            // Change the text of the TextView when the Button is clicked
+            textView.text = "Button Clicked!"
         }
     }
 }
